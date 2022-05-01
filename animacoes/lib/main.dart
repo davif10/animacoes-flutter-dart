f40bf90947c22448150e47209e76bc290a50eb34
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2022-05-01 20:34:58 -0300

    Utilizando Animated Builder

--- animacoes/lib/main.dart
+++ animacoes/lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:animacoes/AnimacaoTween.dart';
-import 'package:animacoes/AnimacoesExplicitasConstruidas.dart';
-import 'package:animacoes/CriandoAnimacoesBasicas.dart';
+
+import 'package:animacoes/MaisSobreAnimacoes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: AnimacoesExplicitasConstruidas(),
+    home: MaisSobreAnimacoes(),
     debugShowCheckedModeBanner: false,
   ));
 }
